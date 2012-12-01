680b0ae005dbd29ff480874ce2e6e2d9588e1574
mai128 <mai128@uom.gr>
2012-12-01 19:44:36 +0200

    initial queries to DB

---
+++ queries.sql
@@ -0,0 +1,17 @@
+#queries to table categoryOfPlays
+
+select * from categoryOfPlays where DIFF=1
+select ID from categoryOfPlays where DIFF=1
+
+select * from categoryOfPlays where DIFF=2
+select ID from categoryOfPlays where DIFF=2
+
+select * from categoryOfPlays where DIFF=3
+select ID from categoryOfPlays where DIFF=3
+
+#queries to table indexOfPlays
+
+select category from indexOfPlays where DIFF=1
+select category from indexOfPlays where DIFF=2
+select category from indexOfPlays where DIFF=3
+
