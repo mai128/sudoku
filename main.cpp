4cd3a8e0f93e1963bcf3e47302a7e6bbe5fa1d70
mai128 <mai128@uom.gr>
2012-12-08 17:27:27 +0200

    create new main

--- main.cpp
+++ main.cpp
@@ -1,48 +1,14 @@
-#include <stdio.h>
-#include "sudoku.h"
-#include "stdlib.h"
+#include <QtGui/QApplication>
+#include "mainwindow.h"
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
-   int r;
-   int c;
-   int value=10;
+    QApplication a(argc, argv);
+   // QTextCodec *utfCodec =QTextCodec::codecForName("UTF-8");
+    //QTextCodec::setCodecForTr(utfCodec);
+   // QTextCodec::setCodecForCStrings(utfCodec);
+    MainWindow w;
+    w.show();
 
-   int ret;
-
-   Sudoku S;
-   S.initSudoku();
-
-   while(value>=0){
-      	system("clear");
-
-        S.showSudoku();
-        if(S.checkSudoku()!=0){
-           printf("\nCheck for error\n");
-        }else{
-           printf("\n\n");
-        }
-
-        if(S.completed()==81 && S.checkSudoku()==0){
-            printf("\Bravo!!!\n");
-            break;
-        }
-
-        printf("\nCompleted %d of 81\n",S.completed());
-        do{
-        printf("\nGive row [0-8]: ");
-        scanf("%d",&r);
-        } while (r<0 || r>8);
-        do{
-        printf("\nGive Column [0-8]: ");
-        scanf("%d",&c);
-        } while (r<0 || c>8);
-       do{
-        printf("\nGive value [0-9] or -1 for exit.\nCell[%d][%d]: ",r,c);
-        scanf("%d",&value);
-        S.setValue(r,c,value);
-        } while (value <-1 || value>9);
-   }
-
-    return 0;
+    return a.exec();
 }
