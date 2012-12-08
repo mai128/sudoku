6c41c04fbf58ce7196fe06816804053e5e64cd7e
mai128 <mai128@uom.gr>
2012-12-08 18:09:39 +0200

    delete comments from class files

--- sudokuform.h
+++ sudokuform.h
@@ -1,30 +1,3 @@
-/***************************************************************************
- **  The PROJECT_NAME project is a set of free, open source
- **  applications.
- **  (C) 2010-2012 by PERIKLIS KIORPES(Greece) <periklis.kiorpes@gmail.com>
- **  All rights reserved.
- **
- **  This program is free software: you can redistribute it and/or modify
- **  it under the terms of the GNU General Public License as published by
- **  the Free Software Foundation, either version 3 of the License, or
- **  (at your option) any later version.
- **
- **  This program is distributed in the hope that it will be useful,
- **  but WITHOUT ANY WARRANTY; without even the implied warranty of
- **  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- **  GNU General Public License for more details.
- **
- **  You should have received a copy of the GNU General Public License
- **  along with this program (COPYING.PROJECT_NAME file).
- **  If not, see <http://www.gnu.org/licenses/>.
- ***************************************************************************/
-/***************************************************************************
- **	 CLASS    : SudokuForm
- **  FILENAME : sudokuform.h
- **  DATE     : 29/11/2012
- **	 USER     : periklis
- ***************************************************************************/
-
 #ifndef SUDOKUFORM_H
 #define SUDOKUFORM_H
 
