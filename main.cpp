1d769e5f260348b317b25de1374de37f9ef152f1
mai128 <mai128@uom.gr>
2012-12-10 22:30:56 +0200

    create new main which supports greek's

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include <QtGui/QApplication>
+#include <QDebug>
+#include <QTextCodec>
+
 #include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-   // QTextCodec *utfCodec =QTextCodec::codecForName("UTF-8");
-    //QTextCodec::setCodecForTr(utfCodec);
+    QTextCodec *utfCodec =QTextCodec::codecForName("UTF-8");
+    QTextCodec::setCodecForTr(utfCodec);
    // QTextCodec::setCodecForCStrings(utfCodec);
+
     MainWindow w;
     w.show();
 
